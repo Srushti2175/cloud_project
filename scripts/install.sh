eaf6961a12108449952dc293ccd49ed2cf2ca942
sneh913 <snehalmane083@gmail.com>
2026-04-29 14:43:27 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,9 +5,7 @@ cd /home/ec2-user/app
 
 npm install
 
-# install globally for ec2-user (NOT sudo)
 npm install -g tsx
 npm install -g pm2
 
-# ensure pm2 is in PATH
 export PATH=$PATH:/usr/local/bin
