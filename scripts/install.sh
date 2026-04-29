9f690e77bf2b5661ba8cd8644ff8874f55097b5d
sneh913 <snehalmane083@gmail.com>
2026-04-29 14:47:52 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+source /etc/profile
+export PATH=$PATH:/usr/local/bin
+
 cd /home/ec2-user/app
 
 npm install
 
 npm install -g tsx
 npm install -g pm2
-
-export PATH=$PATH:/usr/local/bin
